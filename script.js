4c252ecfcc9d44bfa3a5c28ef1eda03930180eea
PandeyKiran7 <kiranmmm138@gmail.com>
2024-08-03 08:03:15 +0545

    add JavaScript for img Slider

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function imgSlider (anything) {
+document.querySelector('.startbucks').src = anything;
+}
+
+function changeCircleColor(color){
+const circle =document.querySelector('.circle');
+circle.style.background = color;
+}
